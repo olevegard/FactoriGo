ae63e7023ddf184165fb6a827f7b64684928c076
Ole Vegard Mythe Moland <oleveg@gmail.com>
2018-02-03 12:24:29 +0100

    Added gotest
    -A simple file with some assert functions
    -Assert functions print stack on failure

---
+++ objects.go
@@ -0,0 +1,19 @@
+package main
+
+type Inventory struct {
+	iron_ore   int
+	copper_ore int
+
+	iron_plates   int
+	copper_plates int
+}
+
+type Production struct {
+	// Harvested
+	iron_mines   int
+	copper_mines int
+
+	// Smelted
+	iron_smelters   int
+	copper_smelters int
+}
